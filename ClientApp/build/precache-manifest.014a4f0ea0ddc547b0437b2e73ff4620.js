8b6fdf5b58b6192b824ca164ee656e57aa4e4689
fabianeboli <fe6321@gmail.com>
2021-03-12 17:39:36 +0000

    Deploying to gh-pages from  @ 4602a50da9f1585096ebe771b1129d8179a57d6e 🚀

---
+++ ClientApp/build/precache-manifest.014a4f0ea0ddc547b0437b2e73ff4620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd6a594915dcded2c9ea633f95f2cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7207e7f8f6fdb9c7acb6",
+    "url": "/static/css/2.cf29e716.chunk.css"
+  },
+  {
+    "revision": "c91e351218d62386a22c",
+    "url": "/static/css/main.bcd875e5.chunk.css"
+  },
+  {
+    "revision": "7207e7f8f6fdb9c7acb6",
+    "url": "/static/js/2.514dde12.chunk.js"
+  },
+  {
+    "revision": "f0c1093415b06c9dc10783dbb242bbcc",
+    "url": "/static/js/2.514dde12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91e351218d62386a22c",
+    "url": "/static/js/main.d7194751.chunk.js"
+  },
+  {
+    "revision": "7d6d6abcb8884051f009",
+    "url": "/static/js/runtime-main.4003ff0d.js"
+  }
+]);
