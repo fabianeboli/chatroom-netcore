2e76965740138d0efd5a3dc76a0441a3afcb0b5e
fabianeboli <fe6321@gmail.com>
2021-03-12 19:11:25 +0000

    Deploying to gh-pages from  @ eab91c36aa6e0123f551af0f59a7156c84e65c6f 🚀

--- ChatApp/ClientApp/src/services/helperFunctions.ts
+++
@@ -1,6 +0,0 @@
-
-export const emailValidation = (email: string) => {
-    const emailPattern: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const isEmailValid: boolean = emailPattern.test(email.toLowerCase());
-    return isEmailValid;
-};
